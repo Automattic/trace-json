a4494ff098e798bf266d3a716e282a12069cb00b
TJ Holowaychuk <tj@vision-media.ca>
2012-10-26 07:56:10 -0700

    add example

--- examples/simple.js
+++ examples/simple.js
@@ -5,8 +5,29 @@
 
 var Tracer = require('..');
 
-var trace = new Tracer('123', 'req/res');
+var trace = new Tracer('req/res', '123');
 
-trace.start('request', Date.now());
-trace.end('request', Date.now() + 500);
-trace.clear();
+var n = 2000;
+
+// simulate N fake http
+// requests to upload an image,
+// resize, and transfer to s3
+
+while (n--) {
+  // faux http upload
+  var now = Date.now();
+  trace.start('request', now);
+
+  // image resizing
+  trace.start('resize', now += Math.random() * 100);
+
+  // transfer to s3
+  trace.start('s3', now);
+  trace.end('s3', now += Math.random() * 100);
+
+  // resize complete
+  trace.end('resize', now += Math.random() * 50);
+
+  // response flushed
+  trace.end('request', now + 50);
+}
